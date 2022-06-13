a076a309517df41f8a1e3f28e03fe3bea4b22f8d
Tensbeam <tensrrt123@gmail.com>
2022-06-13 12:27:17 +0700

    first commit

---
+++ bin/assing.dart
@@ -0,0 +1,12 @@
+main () {
+  print ("Example of Assignmet Operators") ;
+  int a =10;
+  int b =5;
+  a += b;
+//
+  print("a= =b =$a");//Add and Asing
+
+  a -=b;
+
+  print("a -= b = $a");
+}
---
+++ bin/relational.dart
@@ -0,0 +1,24 @@
+void main(){
+  int a = 30;
+  int b = 20;
+  print("Example of Relational Operators");
+
+  bool res = a>b; //true or false
+  print("a > b = $res");
+
+  bool res1 = a<b;
+  print("a<b = $res1");
+
+  bool res2 = a == b;//false
+  print("a == b = $res2");
+   
+  bool res3 = a >= b;
+  print("a >= b: $res3"); //true
+
+  bool res4 = a<= b;
+  print ("a <= b: $res4");// false
+
+  bool res5 = a !=b;
+  print ("a ! = b: $res5"); // true
+
+}
